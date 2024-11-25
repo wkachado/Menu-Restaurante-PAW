f6ee5304056201fa6d1bad24cfbe5abede4f9937
wkachado <107577520+wkachado@users.noreply.github.com>
2024-11-25 20:35:24 -0300

    Version para PythonAnyWhere

---
+++ MiProyecto/MiProyecto/urls.py
@@ -0,0 +1,25 @@
+"""
+URL configuration for MiProyecto project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin                    #Importa el módulo de administración de Django. Esto es necesario para registrar tus modelos y gestionar tu aplicación a través del panel de administración de Django.
+from django.urls import path, re_path, include      #27- importa path desde django.urls y se incluye re_path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),        
+    re_path('', include('App.urls')),
+
+
+]
